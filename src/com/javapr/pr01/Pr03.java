29cb2dd2e0e05151915415dc3c8c05ad2aff283e
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 11:57:29 +0900

    2nd

--- src/com/javapr/pr01/Pr03.java
+++ src/com/javapr/pr01/Pr03.java
@@ -3,8 +3,16 @@ package com.javapr.pr01;
 public class Pr03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		
+		
+		
+		
+		
+		
+		System.out.print(c);
+		
 	}
 
 }
