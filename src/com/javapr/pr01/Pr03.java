3b41e6fa3d2a18f6c03905c18fc8d61d1df30db0
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 12:50:13 +0900

    2nd

--- src/com/javapr/pr01/Pr03.java
+++ src/com/javapr/pr01/Pr03.java
@@ -3,16 +3,19 @@ package com.javapr.pr01;
 public class Pr03 {
 
 	public static void main(String[] args) {
-		
-		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
-		
-		
-		
-		
-		
-		
+
+		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l' };
+
 		System.out.print(c);
-		
+		System.out.println("\n");
+
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == ' ') {
+				c[i] = ',';
+			}
+			System.out.print(c[i]);
+		}
+
 	}
 
 }
